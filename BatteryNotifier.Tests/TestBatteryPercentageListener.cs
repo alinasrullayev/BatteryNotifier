248264d72cce54a70b43590422491edbe3a63b1e
Ali Nasrullayev <nesrullayev.ali@gmail.com>
2023-04-30 20:44:01 +0400

    Added new test

--- BatteryNotifier.Tests/TestBatteryPercentageListener.cs
+++ BatteryNotifier.Tests/TestBatteryPercentageListener.cs
@@ -25,4 +25,26 @@ public class TestBatteryPercentageListener
         _listener.StartListening(3);
         Assert.True(are.WaitOne(TimeSpan.FromSeconds(1)));
     }
+
+    [Fact]
+    public void TestHighBatteryAlertIsTriggered()
+    {
+        AutoResetEvent are = new AutoResetEvent(false);
+
+        BatteryInformation battery1 = new BatteryInformation();
+        battery1.deviceID = "1";
+        battery1.percentage = 85;
+        battery1.health = "OK";
+        battery1.expectedRunTime = 240;
+        battery1.state = BatteryState.Charging;
+
+        IBatteryRepository repository = new InMemoryBatteryRepository();
+        repository.Add(battery1);
+
+        _listener = new BatteryPercentageListener(repository);
+        _listener.BatteryUpperThresholdReached += (sender, args) => { are.Set(); };
+
+        _listener.StartListening(3);
+        Assert.True(are.WaitOne(TimeSpan.FromSeconds(1)));
+    }
 }
